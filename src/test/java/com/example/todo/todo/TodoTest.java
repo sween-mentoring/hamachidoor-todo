830ff65a9a9f33f50df44abe34d58327cac03367
alirz-pixel <alirce@naver.com>
2023-06-22 23:41:41 -0700

    test(todo): add test for todo modify

--- src/test/java/com/example/todo/todo/TodoTest.java
+++ src/test/java/com/example/todo/todo/TodoTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 
 public class TodoTest {
     private String title = "title";
@@ -14,11 +16,30 @@ public class TodoTest {
     @Test
     @DisplayName("Todo 생성 테스트")
     public void create() {
+        // given
         Todo todo = new Todo(title, content, owner);
+        // when
+        // then
         Assertions.assertThat(todo.getTitle()).isEqualTo(title);
         Assertions.assertThat(todo.getContent()).isEqualTo(content);
         Assertions.assertThat(todo.getOwner()).isEqualTo(owner);
         Assertions.assertThat(todo.getCreateAt()).isNotNull();
         Assertions.assertThat(todo.getUpdateAt()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Todo 수정 테스트")
+    public void modify() {
+        // given
+        Todo todo = new Todo(title, content, owner);
+        Todo modifiedTodo = new Todo("modified title", "modified content", "modified owner");
+        LocalDateTime todoCreateTime = todo.getCreateAt();
+        // when
+        todo.modify(modifiedTodo);
+        // then
+        Assertions.assertThat(todo.getTitle()).isEqualTo(modifiedTodo.getTitle());
+        Assertions.assertThat(todo.getContent()).isEqualTo(modifiedTodo.getContent());
+        Assertions.assertThat(todo.getOwner()).isEqualTo(modifiedTodo.getOwner());
+        Assertions.assertThat(todo.getCreateAt()).isEqualTo(todoCreateTime);
     }
 }
