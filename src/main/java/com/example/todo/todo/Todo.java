9c03f21764cc07b1063515d818cfc899de0289c7
alirz-pixel <alirce@naver.com>
2023-06-22 23:06:28 -0700

    test(todo): add test for todo create

---
+++ src/main/java/com/example/todo/todo/Todo.java
@@ -0,0 +1,5 @@
+package com.example.todo.todo;
+
+
+public class Todo {
+}
---
+++ src/test/java/com/example/todo/todo/TodoTest.java
@@ -0,0 +1,23 @@
+package com.example.todo.todo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class TodoTest {
+    private String title = "title";
+    private String content = "content";
+    private String owner = "owner";
+
+    @DisplayName("Todo 생성 테스트")
+    @Test
+    public void create() {
+        Todo todo = new Todo(title, content, owner);
+        Assertions.assertThat(todo.getTitle()).isEqualTo(title);
+        Assertions.assertThat(todo.getContent()).isEqualTo(content);
+        Assertions.assertThat(todo.getOwner()).isEqualTo(owner);
+        Assertions.assertThat(todo.getCreateAt()).isNotNull();
+        Assertions.assertThat(todo.getUpdateAt()).isNotNull();
+    }
+}
